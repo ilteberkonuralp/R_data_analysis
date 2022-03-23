a54b5b27d8568a167cffa138bf85aea497f50247
ilteberkonuralp <ilteber.konuralp65@gmail.com>
2022-03-23 14:43:09 +0300

    some graphical views was added but the idea was changed, next steps a shiny dashboard will be created and users can be decided.

--- Wholesale customers/wholesaleanalysis.R
+++ Wholesale customers/wholesaleanalysis.R
@@ -11,23 +11,84 @@ library(class)
 wholesaledata <- fread("Wholesale customers data.csv")
 wholesaledata %>% head()
 wholesaledata %>% str()
+wholesaledata %>% summary()
+
+wholesaledata<- tibble(wholesaledata)
+wholesaledata$Region<- ifelse(wholesaledata$Region==1,"Lizbon",
+                              ifelse(wholesaledata$Region==2,"Oporto","Other Region"))
+wholesaledata$Channel <- ifelse(wholesaledata$Channel==1,"Horeca","Retail")
 
 wholesaledata <- wholesaledata %>% 
   mutate(Region=as.factor(Region),Channel=as.factor(Channel))
-wholesaledata<- tibble(wholesaledata)
-
-wholesaledata$Region<- ifelse(wholesaledata$Region==1,"Lizbon",
-       ifelse(wholesaledata$Region==2,"Oporto","Other Region"))
-wholesaledata$Channel <- ifelse(wholesaledata$Channel==1,"Horeca","Retail")
 # Splitting data in two group Train and Test
 dim(wholesaledata)
 (N <- nrow(wholesaledata))
+
 target <- round(0.8*N)
 train_g <- runif(N,0,1)<0.8
 saledata_train <- wholesaledata[train_g,]
 saledata_test <- wholesaledata[!train_g,]
-saledata_train %>% summary()
-dim(saledata_train)
+saledata_train %>% ggplot(aes(x=Channel,fill=Region))+
+  geom_bar()
+# Table format of two categorical columns
+saledata_train %>% select(Channel,Region) %>% table()
+# Proportion of them
+saledata_train %>% select(Channel,Region) %>% table() %>% prop.table()
+# Bar plot which is created side by side to see categorical case
+
+saledata_train %>% ggplot(aes(x=Channel,fill=Region))+
+  geom_bar(position="dodge")+
+  labs(title = "Bar chart of Channel and Region")+
+theme(axis.text.x = element_text(angle=45,size=12,color="black",vjust = -0.05),
+      axis.ticks.x = element_blank())
+# Another bar plot which is based on Region
+saledata_train %>% ggplot(aes(x=Region,fill=Channel))+
+  geom_bar(position="dodge")+
+  theme(axis.text.x = element_text(color="black"))
+# Simple Barcharts
+library("colorRamps")
+saledata_train %>% ggplot(aes(x=Channel))+
+  geom_bar(fill=cm.colors(2,alpha=0.7))
+# Faceting these factor columns 
+saledata_train %>% ggplot(aes(x=Channel))+
+  geom_bar()+
+  facet_grid(~Region)
+# Exploring Numerical data with dotplot
+saledata_train %>% select_if(is.numeric) %>% ggplot(aes(x=Fresh))+
+  geom_dotplot(dotsize=0.1)
+# On the other hand histogram would be great to see distribution of it
+saledata_train %>% select_if(is.numeric) %>% ggplot(aes(x=Milk))+
+  geom_histogram(bins=15)
+# Density plot of the some columns
+saledata_train %>% select_if(is.numeric) %>% ggplot(aes(x=Frozen))+
+  geom_density()
+saledata_train %>% select_if(is.numeric) %>% ggplot(aes(x=Grocery))+
+  geom_density()
+# Finally lets see their boxplots
+saledata_train %>% select_if(is.numeric) %>% ggplot(aes(x=Detergents_Paper))+
+  geom_density()
+saledata_train %>% select_if(is.numeric) %>% ggplot(aes(x=Delicassen))+
+  geom_density()
+# As may seen all of the points are seems they are right skewed forms
+# Transformation may help to normalize these colums or at least may help to
+# understand or make some interpretation.
+# As another view option, faceted histograms may help to view existence of
+# any distribution of them by region or channel
+saledata_train %>% ggplot(aes(x=Milk))+
+  geom_histogram(bins=15)+
+  facet_grid(~Region)
+saledata_train %>% ggplot(aes(x=Milk))+
+  geom_histogram(bins=15)+
+  facet_grid(~Channel)
+names(saledata_train)
+saledata_train %>% ggplot(aes(x=Fresh,fill=Region))+
+  geom_histogram(bins=15)+
+  facet_grid(~Region)
+  
+saledata_train %>% ggplot(aes(x=Fresh,fill=Channel))+
+  geom_histogram(bins=15)+
+  facet_grid(~Channel)
+
 # Created corrplots
 corrplot(saledata_train %>%  select_if(is.numeric)%>% cor(), tl.col = "red", tl.srt = 60, bg = "orange",
          title = "\n\n Correlation Plot Of Wholesale Customers",
